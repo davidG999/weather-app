172894fb2156c4c6a901824efc6c0b2c34efd8f9
DavidGit999 <itprog999@gmail.com>
2022-07-03 22:52:40 +0300

    Add bg assets and styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            name="search-bar"
+            placeholder="Search..." />
+        </div>
+      </main>
     </div>
   );
 }
