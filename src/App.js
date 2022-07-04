62f142a79e96cb9b7524b0482c7975a2db480760
DavidGit999 <itprog999@gmail.com>
2022-07-04 15:53:02 +0300

    Add weather box and style it

--- src/App.js
+++ src/App.js
@@ -33,7 +33,8 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className={
+      (typeof weather.main != 'undefined') ? ((weather.main.temp - 273.15 > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input
@@ -45,10 +46,20 @@ function App() {
             onKeyDown={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location"> New York City, US </div>
-          <div className="date"> {dateBuilder(new Date())} </div>
-        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp - 273.15 /* Kelvin to Celsius */)}°c
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ('')}
       </main>
     </div>
   );
